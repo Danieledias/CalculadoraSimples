f781c613c9a1521e6c9780f4c57b74dbdcfb0457
Danieledias <danie_le_@hotmail.com>
2021-01-25 00:03:55 -0300

    Primeiro commit

---
+++ Calculate.java
@@ -0,0 +1,32 @@
+public class Calculate {
+
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        if (args[0].equals("somar")) {
+            soma(x, y);
+        } else if (args[0].equals("subtrair")){
+            subtrair (x, y);
+        } else if (args[0].equals("dividir")){
+            dividir (x, y);
+        } else if (args[0].equals("multiplicar")){
+            multiplicar (x, y);
+        } else {
+            System.out.println("Nenhuma instrução definida!");
+        }
+    }
+
+    static void soma(int x, int y){
+        System.out.println(x + y);
+    }
+    static void subtrair(int x, int y){
+        System.out.println(x - y);
+    }
+    static void dividir(int x, int y){
+        System.out.println(x / y);
+    }
+    static void multiplicar(int x, int y){
+        System.out.println(x * y);
+    }
+}
